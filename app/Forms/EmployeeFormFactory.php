aa2937ee432d5d4905c9ace7a1ee313846a9d9ba
Fabricio <fabricio872@gmail.com>
2023-10-28 09:45:34 +0200

    updated design to bootstrap 5

--- app/Forms/EmployeeFormFactory.php
+++ app/Forms/EmployeeFormFactory.php
@@ -3,12 +3,14 @@
 namespace App\Forms;
 
 use Nette\Application\UI\Form;
+use Tomaj\Form\Renderer\BootstrapRenderer;
 
 class EmployeeFormFactory
 {
     public function create()
     {
         $form = new Form();
+        $form->setRenderer(new BootstrapRenderer());
         $form->addText('name', 'Name:');
         $form->addText('sex', 'Sex:');
         $form->addInteger('age', 'Age:');
